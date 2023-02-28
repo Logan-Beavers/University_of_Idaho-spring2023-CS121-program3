fb46aa9c40cb6ff5126c0a595abb296a2c4108c3
Logan Beavers <loganbeavers2004@gmail.com>
2023-02-28 12:16:46 -0800

    add a description of program intent

---
+++ arithmetic-infix-to-postfix.cpp
@@ -0,0 +1,11 @@
+/* arithmetic-infix-to-postfix.cpp
+ *
+ * CS 121.Bolden.........MSVC++ 19.34.31937...........Logan Beavers
+ *						 G++ 10.2.1
+ * 2023/2/28.................Intel 9100F.............Beav6334@vandals.uidaho.edu
+ *							 AMD FX-9800P
+ *
+ * Programming Assignment #3
+ * Take in an infix operation, then check it for errors, then convert it to postfix, then evaluate the expression.
+ *---------------------------------------------------------------------
+ */
