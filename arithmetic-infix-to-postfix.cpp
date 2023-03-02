bf558947d016c29452f690f2bf4ef7383cee018b
Logan Beavers <loganbeavers2004@gmail.com>
2023-03-01 22:27:30 -0800

    update date and add program entry point

--- arithmetic-infix-to-postfix.cpp
+++ arithmetic-infix-to-postfix.cpp
@@ -2,10 +2,18 @@
  *
  * CS 121.Bolden.........MSVC++ 19.34.31937...........Logan Beavers
  *						 G++ 10.2.1
- * 2023/2/28.................Intel 9100F.............Beav6334@vandals.uidaho.edu
- *							 AMD FX-9800P
+ * 2023/5/1.................Intel 9100F.............Beav6334@vandals.uidaho.edu
+ *							AMD FX-9800P
  *
  * Programming Assignment #3
  * Take in an infix operation, then check it for errors, then convert it to postfix, then evaluate the expression.
  *---------------------------------------------------------------------
  */
+
+#include <cstdlib>
+
+
+int main(int argc, char* argv[])
+{
+	return EXIT_SUCCESS;
+}
